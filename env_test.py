dbeec126b42ebde7fc952f906767db025668b75e
hosein <bahadorm@informatik.uni-freiburg.de>
2019-06-08 18:31:45 +0200

    not working env

--- env_test.py
+++ env_test.py
@@ -1,20 +1,51 @@
+import numpy as np
 import gym
 import MADRaS
+import sys
+import time
+from pyglet.window import key
+
+def key_press(k, mod):
+    global restart
+    if k == 0xff0d: restart = True
+    if k == key.LEFT:  action[0] = -1.0
+    if k == key.RIGHT: action[0] = +1.0
+    if k == key.UP:    action[1] = +1.0
+    if k == key.DOWN:  action[2] = +0.2
+
+def key_release(k, mod):
+    if k == key.LEFT and action[0] == -1.0: action[0] = 0.0
+    if k == key.RIGHT and action[0] == +1.0: action[0] = 0.0
+    if k == key.UP:    action[1] = 0.0
+    if k == key.DOWN:  action[2] = 0.0
 
 
+if __name__ == "__main__":
+    
+    # set up the environment
+    env = gym.make('Madras-v0', throttle=True, vision=True, visualise=True, port=3001, pid_assist=False)
+    print ("env port : {}".format(env.port))
+    #env.viewer.window.on_key_press = key_press
+    #env.viewer.window.on_key_release = key_release
+    action = np.array([0.0, 0.0, 0.0]).astype('float32')
+    episode = 0
+    
+    while True:
+        obs = env.reset()
+        step = 0
+        episode_reward = 0
+        while True:
+            # u[0] = steer, u[1] = accel, u[2] = brake
+            obs, rew, done, info = env.step(action)
+            
+            episode_reward += rew
+            step +=1
 
-env = gym.make('Madras-v0', throttle=True, vision=True, visualise=True, port=60934, pid_assist=False)
+            env.render()
 
+            if done :
+                break
 
-num_episodes = 10000
-num_steps = 5000
+        episode += 1
 
-
-for i in range(num_episodes):
-    env.reset()
-    for i in range(num_steps):
-        # u[0] = steer, u[1] = accel, u[2] = break
-        a = policy(obs)
-        obs1, rew, done, info = env.step(a)
-
-
+    env.close()
