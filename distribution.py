b216449d78f2c7fe3d6c1e225aa2328f4f3dc6b8
hosein <bahadorm@informatik.uni-freiburg.de>
2019-07-06 13:15:41 +0200

    adapted dist.

--- distribution.py
+++ distribution.py
@@ -5,7 +5,7 @@ import os
 import torch
 import torch.utils.data
 import torchvision.transforms as transforms
-from utils import action_to_label
+from utils import action_to_label_double
 
 DATASET_DIR = "/home/bahadorm"
 PKG_NAME = "carla_dataset.hdf5"
@@ -15,13 +15,13 @@ data = h5py.File(os.path.join(DATASET_DIR, PKG_NAME), "r")
 keys = list(data.keys())
 ds_count = len(keys)
 
-count  = np.zeros((9))
+count  = np.zeros((3))
 
 for i in range(ds_count):
     episode = data[keys[i]]
     print("episode {}/{}".format(i,ds_count))
     for j in range(episode.shape[0]) :
-        label = action_to_label(episode[j, "label"])
+        label, _ = action_to_label_double(episode[j, "label"])
         count[label] +=1
 
 print(count)
