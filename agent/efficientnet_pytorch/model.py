495bb4af0b2c8be2c7e686e3b8bb7b0ee6648816
hosein <bahadorm@informatik.uni-freiburg.de>
2019-07-06 13:32:23 +0200

    torch.tanh used

--- agent/efficientnet_pytorch/model.py
+++ agent/efficientnet_pytorch/model.py
@@ -185,7 +185,7 @@ class EfficientNet(nn.Module):
 
         if self.double :
             x_class = self._fc_classification(x)
-            x_regress = F.tanh(self._fc_regression(x))
+            x_regress = torch.tanh(self._fc_regression(x))
             return x_class, x_regress
         else :
             x = self._fc_classification(x)
