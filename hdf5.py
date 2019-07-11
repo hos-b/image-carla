df284d95f99b5196670a68fe98b00cf838656f32
hexed <hexed@outlook.com>
2019-07-11 11:55:11 +0200

    retarded

--- hdf5.py
+++ hdf5.py
@@ -1,13 +1,12 @@
 import numpy as np
 import h5py
 
-        data = h5py.File("tmp/carla_dataset.hdf5", "r")        
-        keys = list(data.keys())
-        ds_count = len(keys)
+data = h5py.File("tmp/carla_dataset.hdf5", "r")        
+keys = list(data.keys())
+ds_count = len(keys)
 
-        for index in range(ds_count):
-            print("{} : {} frames".format(keys[index] ,data[keys[index]].shape[0]))
-            runsum += sizes[index]
-            
-        data.close()
-        data=None
+for index in range(ds_count):
+    print("{} : {} frames".format(keys[index] ,data[keys[index]].shape[0]))
+
+data.close()
+data=None
