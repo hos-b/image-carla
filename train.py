ee78ce745998ee35070953dcc79a3bc74840a7ef
hosein <bahadorm@informatik.uni-freiburg.de>
2019-07-17 00:27:35 +0200

    fixed

--- train.py
+++ train.py
@@ -128,7 +128,6 @@ for epoch in range(args.start_epoch, args.num_epochs+1):
         training_accuracy_reg +=reg_accuracy
         writer.add_scalar("iteration/trn_classification", loss_cls.item(), (epoch-1)*len(train_loader)+idx)
         writer.add_scalar("iteration/trn_regression", loss_reg.item(), (epoch-1)*len(train_loader)+idx)
-        break
     training_accuracy_cls /= len(train_loader)
     training_accuracy_reg /= len(train_loader)
     writer.add_scalar("training/regression", reg_loss_t/len(train_loader), epoch)
@@ -190,7 +189,6 @@ for epoch in range(args.start_epoch, args.num_epochs+1):
         cls_accuracy, reg_accuracy = batch_accuracy(pred_cls,pred_reg,labels.squeeze(1),steer)
         validation_accuracy_cls +=cls_accuracy
         validation_accuracy_reg +=reg_accuracy
-        break
     validation_accuracy_cls /= len(val_loader)
     validation_accuracy_reg /= len(val_loader)
     # saving current val loss as a shitty way of saving 'good' models
